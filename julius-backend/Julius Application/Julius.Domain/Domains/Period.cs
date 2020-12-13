1ebde8208d2348bf721a82cde35ec223083df772
Eriel Miquilino Pereira <erielmiquilino@hotmail.com>
2020-12-12 23:32:55 -0300

    Criação da entidade Period para emcapsular os periodos de listagem

---
+++ julius-backend/Julius Application/Julius.Domain/Domains/Period.cs
@@ -0,0 +1,12 @@
+﻿using Julius.Domain.Domains.Base;
+
+namespace Julius.Domain.Domains
+{
+    public class Period : BaseEntity
+    {
+        public string Month { get; set; }
+
+        public string Year { get; set; }
+
+    }
+}
